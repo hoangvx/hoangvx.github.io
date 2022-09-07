7b5afa728a1964070dfc9f919b279a3b4d4d61bc
kyo <hoang.vx.github@outlook.jp>
2022-09-07 14:01:33 +0000

    first commit for gatsby ts and tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import type { HeadFC } from "gatsby"
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
