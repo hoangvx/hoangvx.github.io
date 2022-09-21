fc163475f74a9b6090359dfedaac2f8dc89cb0a1
hoang.vx <hoang.vx@glv.co.jp>
2022-09-21 09:29:02 +0000

    add image and work style

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,9 +6,6 @@ const config: GatsbyConfig = {
     siteUrl: `https://hoangvx.github.io/`,
     description: `HoangVX's page!`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     {
